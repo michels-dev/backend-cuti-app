536c213a98036b70c0b26509dad044e4e1dc2ca3
michelsdev <sinambelamichel05@gmail.com>
2024-03-06 15:04:17 +0700

    Kommit keempat DTO

--- src/cuti/dto/create-cuti.dto.ts
+++ src/cuti/dto/create-cuti.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCutiDto {}
+export class CreateCutiDto {
+
+  // create DTO cuti
+  nik : number;
+  user_created : string;
+  tanggal_mulai : Date;
+  tanggal_selesai : Date;
+  approval : number | null;
+  approval_date : Date;
+  keperluan : string;
+  tipe : string;
+  pengganti : string;
+}
+
+export default CreateCutiDto
+
--- src/cuti/dto/update-cuti.dto.ts
+++ src/cuti/dto/update-cuti.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCutiDto } from './create-cuti.dto';
 
-export class UpdateCutiDto extends PartialType(CreateCutiDto) {}
+export class UpdateCutiDto extends PartialType(CreateCutiDto) {
+
+  // update DTO cuti
+  id: string;
+  nik : number;
+  user_created : string;
+  tanggal_mulai : Date;
+  tanggal_selesai : Date;
+  approval : number | null;
+  approval_date : Date;
+  keperluan : string;
+  tipe : string;
+  pengganti : string;
+}
+
+export default UpdateCutiDto;
