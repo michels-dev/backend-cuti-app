00044d749dbe13316aed718fd9dcfd0da4a8bf2b
michelsdev <sinambelamichel05@gmail.com>
2024-03-06 15:18:36 +0700

    Kommit kelima Entity

--- src/cuti/entities/cuti.entity.ts
+++ src/cuti/entities/cuti.entity.ts
@@ -1,1 +1,29 @@
-export class Cuti {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Cuti {
+  // create Entity cuti
+  @PrimaryGeneratedColumn("uuid")
+  id : string;
+
+  @Column()
+  tanggal_mulai : Date;
+
+  @Column()
+  tanggal_selesai : Date;
+
+  @Column({ nullable : true })
+  approval : number;
+
+  @Column()
+  approval_date : Date;
+
+  @Column()
+  keperluan : string;
+
+  @Column()
+  tipe : string;
+
+  @Column()
+  pengganti : string;
+}
