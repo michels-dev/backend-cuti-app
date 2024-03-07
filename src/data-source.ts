ba93f5325918c5fd188b35c974e420bc9a3067e6
michelsdev <sinambelamichel05@gmail.com>
2024-03-07 10:48:56 +0700

    Kommit keenam Data Source

---
+++ src/data-source.ts
@@ -0,0 +1,11 @@
+import { DataSource } from "typeorm";
+
+const DataSourceCuti = new DataSource({
+  // Data Source Migrations
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "michels05",
+  entities: ["src/cuti/entities/**/*.entity{.ts,.js}"],
+})
